aadef0ab1b4f6638ff0bcdc7c9fa70d3eeef70be
Nate Vogel <nathan.c.vogel@gmail.com>
2020-04-27 08:48:13 -0400

    moved questions into a subcomponent folder, and created a folder to hold components that are explanations of programs that will appear at the end of interviews

--- src/components/questions/ChildCareFMLAAndUC.js
+++ src/components/questions/ChildCareFMLAAndUC.js
@@ -1,5 +1,5 @@
 import React from "react";
-import MultipleChoice from "./MultipleChoice";
+import MultipleChoice from "../MultipleChoice";
 
 /**
  * Questions relating to eligibility for childcare-related FMLA and UC.
