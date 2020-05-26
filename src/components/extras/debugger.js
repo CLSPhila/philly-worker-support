78505d7f8b7ea65987cfc8614bd971b723fdc00f
Nate Vogel <nathan.c.vogel@gmail.com>
2020-05-26 11:03:24 -0400

    simplified debugger output, so i can copy-paste into tests.

--- src/components/extras/debugger.js
+++ src/components/extras/debugger.js
@@ -1,5 +1,19 @@
 import React from "react";
 
+const simplifyState = (state) => {
+  const { answers, interviewHistory } = state;
+  let simplifiedAnswers = {};
+  for (let questionId in answers) {
+    if (answers[questionId].answer !== null) {
+      simplifiedAnswers[questionId] = answers[questionId].answer;
+    }
+  }
+  return {
+    answers: simplifiedAnswers,
+    interviewHistory,
+  };
+};
+
 export const Debugger = (props) => {
   const { state } = props;
   return (
@@ -7,7 +21,7 @@ export const Debugger = (props) => {
       style={{ backgroundColor: "grey", padding: "0.5rem", marginTop: "2rem" }}
     >
       <p>Debugging info for interview state:</p>
-      <pre>{JSON.stringify(state, null, 4)}</pre>
+      <pre>{JSON.stringify(simplifyState(state), null, 4)}</pre>
     </div>
   );
 };
