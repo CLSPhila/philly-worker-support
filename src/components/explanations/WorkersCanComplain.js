1b3ae98df8552b17494d5ece688b4edcc932d65e
NateV <nathan.c.vogel@gmail.com>
2020-05-28 20:44:43 -0400

    styling tweaks, and working on the ContactCLS component, which still needs work

--- src/components/explanations/WorkersCanComplain.js
+++ src/components/explanations/WorkersCanComplain.js
@@ -1,17 +1,22 @@
 import React from "react";
 
 import { Link } from "react-router-dom";
+import { ContactCLS } from "../extras/ContactCLS";
 export const WorkersRightToComplain = () => {
   return (
     <div>
       <p>
-        Workers’ rights to complain/refuse unsafe work are sometimes protected.
-        Call CLS!
+        Workers have rights to complain or refuse unsafe work. We may be able to
+        provide you with more information about your specific circumstances.
+        Give us a call.
       </p>
+      <ContactCLS />
       <p>
-        If you haven't already, click to learn about eligibility for
-        unemployment compensation and Pandemic Unemployment Assistance (PUA):
-        <Link to="/questions/uc-pua-benefits"> here. </Link>
+        If you haven't already, click to{" "}
+        <Link to="/questions/uc-pua-benefits">
+          learn about eligibility for unemployment compensation and Pandemic
+          Unemployment Assistance (PUA){" "}
+        </Link>
       </p>
     </div>
   );
